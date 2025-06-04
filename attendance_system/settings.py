639be0e37a40511b2a5578daddd00e4f6313f0ee
chandhini-2412 <142402617+chandhini-2412@users.noreply.github.com>
2025-06-04 17:18:19 +0530

    Update settings.py

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -91,15 +91,12 @@ LOGOUT_REDIRECT_URL = 'login'  # or any other URL name or path
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
         'HOST': 'bigfixdb.mysql.database.azure.com',
         'USER': 'dbadmin',
         'PASSWORD': 'Ud4Fn685jQn2A9XGyiYx',
         'NAME': 'Bigfix',
         'PORT': '3306',
-        'OPTIONS': {
-            'ssl_ca': os.path.join(BASE_DIR, 'certs', 'DigiCertGlobalRootG2.crt.pem'),
-        }
     }
 }
 
